374b35270d6abf305d8565058a86b678bb5119d2
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2023-04-18 20:12:08 +0200

    Batch-Obfuscator-VB
    Batch-Obfuscator-VB

---
+++ BatchIDE/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿Namespace BatchIDE
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.textBox1 = New System.Windows.Forms.TextBox()
+			Me.checkBox1 = New System.Windows.Forms.CheckBox()
+			Me.checkBox2 = New System.Windows.Forms.CheckBox()
+			Me.button1 = New System.Windows.Forms.Button()
+			Me.textBox2 = New System.Windows.Forms.TextBox()
+			Me.label1 = New System.Windows.Forms.Label()
+			Me.checkBox3 = New System.Windows.Forms.CheckBox()
+			Me.groupBox1 = New System.Windows.Forms.GroupBox()
+			Me.groupBox1.SuspendLayout()
+			Me.SuspendLayout()
+			' 
+			' textBox1
+			' 
+			Me.textBox1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.textBox1.BackColor = System.Drawing.Color.Gray
+			Me.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+			Me.textBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.textBox1.ForeColor = System.Drawing.Color.White
+			Me.textBox1.Location = New System.Drawing.Point(9, 12)
+			Me.textBox1.Multiline = True
+			Me.textBox1.Name = "textBox1"
+			Me.textBox1.Size = New System.Drawing.Size(652, 445)
+			Me.textBox1.TabIndex = 0
+			' 
+			' checkBox1
+			' 
+			Me.checkBox1.AutoSize = True
+			Me.checkBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.checkBox1.ForeColor = System.Drawing.Color.Black
+			Me.checkBox1.Location = New System.Drawing.Point(16, 19)
+			Me.checkBox1.Name = "checkBox1"
+			Me.checkBox1.Size = New System.Drawing.Size(205, 25)
+			Me.checkBox1.TabIndex = 1
+			Me.checkBox1.Text = "Use Base64 Obfuscation"
+			Me.checkBox1.UseVisualStyleBackColor = True
+			' 
+			' checkBox2
+			' 
+			Me.checkBox2.AutoSize = True
+			Me.checkBox2.Font = New System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.checkBox2.ForeColor = System.Drawing.Color.Black
+			Me.checkBox2.Location = New System.Drawing.Point(16, 40)
+			Me.checkBox2.Name = "checkBox2"
+			Me.checkBox2.Size = New System.Drawing.Size(222, 25)
+			Me.checkBox2.TabIndex = 2
+			Me.checkBox2.Text = "Use Junk Data Obfuscation"
+			Me.checkBox2.UseVisualStyleBackColor = True
+			' 
+			' button1
+			' 
+			Me.button1.Anchor = (CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.button1.BackColor = System.Drawing.Color.White
+			Me.button1.Font = New System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.button1.ForeColor = System.Drawing.Color.Black
+			Me.button1.Location = New System.Drawing.Point(809, 479)
+			Me.button1.Name = "button1"
+			Me.button1.Size = New System.Drawing.Size(145, 40)
+			Me.button1.TabIndex = 3
+			Me.button1.Text = "Compile"
+			Me.button1.UseVisualStyleBackColor = False
+'			Me.button1.Click += New System.EventHandler(Me.button1_Click)
+			' 
+			' textBox2
+			' 
+			Me.textBox2.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.textBox2.BackColor = System.Drawing.SystemColors.ControlLight
+			Me.textBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.textBox2.ForeColor = System.Drawing.Color.Black
+			Me.textBox2.Location = New System.Drawing.Point(101, 487)
+			Me.textBox2.Name = "textBox2"
+			Me.textBox2.Size = New System.Drawing.Size(702, 26)
+			Me.textBox2.TabIndex = 6
+			' 
+			' label1
+			' 
+			Me.label1.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.label1.AutoSize = True
+			Me.label1.Font = New System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.label1.ForeColor = System.Drawing.Color.Black
+			Me.label1.Location = New System.Drawing.Point(16, 489)
+			Me.label1.Name = "label1"
+			Me.label1.Size = New System.Drawing.Size(86, 21)
+			Me.label1.TabIndex = 7
+			Me.label1.Text = "File Name:"
+			' 
+			' checkBox3
+			' 
+			Me.checkBox3.AutoSize = True
+			Me.checkBox3.Font = New System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
+			Me.checkBox3.ForeColor = System.Drawing.Color.Black
+			Me.checkBox3.Location = New System.Drawing.Point(16, 61)
+			Me.checkBox3.Name = "checkBox3"
+			Me.checkBox3.Size = New System.Drawing.Size(142, 25)
+			Me.checkBox3.TabIndex = 9
+			Me.checkBox3.Text = "Compile C# File"
+			Me.checkBox3.UseVisualStyleBackColor = True
+			' 
+			' groupBox1
+			' 
+			Me.groupBox1.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+			Me.groupBox1.Controls.Add(Me.checkBox1)
+			Me.groupBox1.Controls.Add(Me.checkBox3)
+			Me.groupBox1.Controls.Add(Me.checkBox2)
+			Me.groupBox1.Location = New System.Drawing.Point(676, 7)
+			Me.groupBox1.Name = "groupBox1"
+			Me.groupBox1.Size = New System.Drawing.Size(278, 450)
+			Me.groupBox1.TabIndex = 10
+			Me.groupBox1.TabStop = False
+			Me.groupBox1.Text = "Settings"
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.BackColor = System.Drawing.SystemColors.ControlDark
+			Me.ClientSize = New System.Drawing.Size(975, 547)
+			Me.Controls.Add(Me.groupBox1)
+			Me.Controls.Add(Me.button1)
+			Me.Controls.Add(Me.textBox1)
+			Me.Controls.Add(Me.textBox2)
+			Me.Controls.Add(Me.label1)
+			Me.MinimumSize = New System.Drawing.Size(991, 583)
+			Me.Name = "Form1"
+			Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+			Me.Text = "Batchfuscator"
+'			Me.Load += New System.EventHandler(Me.Form1_Load)
+			Me.groupBox1.ResumeLayout(False)
+			Me.groupBox1.PerformLayout()
+			Me.ResumeLayout(False)
+			Me.PerformLayout()
+
+		End Sub
+
+		#End Region
+
+		Private textBox1 As System.Windows.Forms.TextBox
+		Private checkBox1 As System.Windows.Forms.CheckBox
+		Private checkBox2 As System.Windows.Forms.CheckBox
+		Private WithEvents button1 As System.Windows.Forms.Button
+		Private textBox2 As System.Windows.Forms.TextBox
+		Private label1 As System.Windows.Forms.Label
+		Private checkBox3 As System.Windows.Forms.CheckBox
+		Private groupBox1 As System.Windows.Forms.GroupBox
+	End Class
+End Namespace
+
---
+++ BatchIDE/Form1.vb
@@ -0,0 +1,90 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+Imports System.IO
+Imports System.Diagnostics
+Imports CodeTools
+Namespace BatchIDE
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+
+		Private Shared random As New Random()
+		Public Shared Function RandomString(ByVal length As Integer) As String
+			Const chars As String = "abcdefghijklmnopqrstuvwxyz"
+			Return New String(Enumerable.Repeat(chars, length).Select(Function(s) s(random.Next(s.Length))).ToArray())
+		End Function
+		Public output As String
+		Public output2 As String
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			If textBox2.Text = "" Then
+				MessageBox.Show("Please enter a file name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			Else
+			   ' try
+					If checkBox2.Checked = True Then
+						Dim input = textBox1.Text
+						Dim bruh As String = input = String.Concat(input.Select(Function(x)If(Char.IsLetterOrDigit(x), "%" & RandomString(6) & "%" & x, x.ToString()))).TrimStart()
+						output = bruh
+					ElseIf checkBox2.Checked = False Then
+						output = textBox1.Text
+					End If
+					If checkBox1.Checked = True Then
+						Dim p1 = output & ControlChars.Lf & "Del %~0"
+						Dim plainTextBytes = System.Text.Encoding.UTF8.GetBytes(p1)
+						Dim outy = System.Convert.ToBase64String(plainTextBytes)
+
+						Dim sb As String = "@echo off" & ControlChars.Lf & "CERTUTIL -f -decode ""%~f0"" ""%Temp%/test.bat"" >nul 2>&1 " & ControlChars.Lf & "cls" & ControlChars.Lf & """%Temp%/test.bat""" & ControlChars.Lf & "Exit" & ControlChars.Lf & "-----BEGIN CERTIFICATE-----" & ControlChars.Lf & outy & ControlChars.Lf & "-----END CERTIFICATE-----"
+						File.WriteAllText(Environment.CurrentDirectory & "/" & textBox2.Text & ".bat", sb)
+						output = sb
+
+					End If
+					If checkBox3.Checked = True Then
+						Dim outy As String = output & ControlChars.Lf & "Del %~0"
+						Dim configData1 As String = File.ReadAllText(Environment.CurrentDirectory & "/config.txt")
+						Dim configData2 As String = File.ReadAllText(Environment.CurrentDirectory & "/config2.txt")
+						Dim o As New Obfuscator()
+						Dim out2 As String = o.Obfuscate(outy, "123")
+						Dim config As String = $"{out2}"
+						Dim conjoined As String = configData1 & Environment.NewLine & config & configData2
+
+						File.WriteAllText(Environment.CurrentDirectory & "/" & textBox2.Text & ".cs", conjoined)
+'                        /
+'                        Process pr = new Process();
+'                        Process process = new Process();
+'                        ProcessStartInfo startInfo = new ProcessStartInfo();
+'                        startInfo.FileName = Environment.CurrentDirectory + "/csc.exe";
+'                        startInfo.Arguments = textBox2.Text + ".cs";
+'                        process.StartInfo = startInfo;
+'                        process.Start();
+'                        /
+						MessageBox.Show("Successfully Generated C# File. Now Making Batch File.", "Success!" & MessageBoxIcon.Information)
+
+
+
+
+
+					End If
+					File.WriteAllText(Environment.CurrentDirectory & "/" & textBox2.Text & ".bat", output)
+
+					MessageBox.Show("Obfuscated Successfully", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+				'catch { MessageBox.Show("Fatal Error Occured", "Error"); }
+			End If
+
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+
+		End Sub
+	End Class
+End Namespace
